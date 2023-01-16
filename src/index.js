99c439d4f5650c1defd2b6596ccc9afc2d2336be
damilolaakinbi <akinbidamilola074@gmail.com>
2023-01-16 12:54:59 +0100

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const user_input = e.target["new-task-description"].value;
+    console.log(user_input);
+    form.reset();
+
+    const ul = document.getElementById("tasks");
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+
+    btn.textContent = `x`;
+    li.textContent = `${user_input} `;
+    li.appendChild(btn);
+    ul.appendChild(li);
+
+    btn.addEventListener("click", (e) => e.target.parentNode.remove());
+  });
 });
